d438b926a01d6be165d04694a58d8a7e415e868a
Khalid G <kgibson4795@gmail.com>
2023-07-26 23:45:22 -0400

    followed tutorial and created some values and started the first function

--- script.js
+++ script.js
@@ -3,24 +3,42 @@ let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 
-let lowerAlphabetExample = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let upperAlphabetExample = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-let numbersExample = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-let specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', '[', ']', ':', ';', ',', '.', '/', '?', '>', '<']
+let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+// let specialCharacters = ['~','!','@','#','$','%','^','&','*','_','-','+','=','\','<','>','.','?','/']
 
-
+ 
+let builtArray = []
 
 
 
 function generatePassword(){
 
-
   if(confirm("Do you want your password to contain lowercase letters?"))
+  {
+
+    builtArray = [...builtArray, ...lowerAlphabet]
+
+  }
+  
+let password = "";
+
+  
+let passwordLength = prompt("how long do you want your password to be?")
+  
+  for (let i = 0; i < passwordLength; i++) {
+    
+    password += "x";
+    
+  }
+
+  
 
 //building a string based off of randomly selected values from an array
 
 
-return "password example"
+return password;
 
 }
 
