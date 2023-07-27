505855ad4edf27b53dcd90117677e796df8e3911
Khalid G <kgibson4795@gmail.com>
2023-07-27 14:35:33 -0400

    final touches on the challenge. added all the if statements. added a empty built array at end of function  to reset values each time. added correct special characters. added letter index for random generated values and correctly linked everything

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ let generateBtn = document.querySelector("#generate");
 let lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let numbers = [0,1,2,3,4,5,6,7,8,9];
-// let specialCharacters = ['~','!','@','#','$','%','^','&','*','_','-','+','=','\','<','>','.','?','/']
+let SpecialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', "[", '\\', "]", '^', "_", '`', '{', '|', '}', '~'];
 
- 
+
+
+
 let builtArray = []
 
 
@@ -21,6 +23,31 @@ function generatePassword(){
     builtArray = [...builtArray, ...lowerAlphabet]
 
   }
+
+  if(confirm("Do you want your password to contain uppercase letters?"))
+  {
+
+    builtArray = [...builtArray, ...upperAlphabet]
+
+  }
+
+  if(confirm("Do you want your password to contain numbers?"))
+  {
+
+    builtArray = [...builtArray, ...numbers]
+
+  }
+
+  if(confirm("Do you want your password to contain special characters?"))
+  {
+
+    builtArray = [...builtArray, ...SpecialCharacters]
+
+  }
+
+
+
+  
   
 let password = "";
 
@@ -28,12 +55,13 @@ let password = "";
 let passwordLength = prompt("how long do you want your password to be?")
   
   for (let i = 0; i < passwordLength; i++) {
-    
-    password += "x";
+    let generatedLetterIndex = Math.floor(Math.random()*builtArray.length)
+
+    password += builtArray[generatedLetterIndex];
     
   }
 
-  
+  builtArray = []
 
 //building a string based off of randomly selected values from an array
 
